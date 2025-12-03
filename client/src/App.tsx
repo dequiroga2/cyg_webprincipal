bd21e71d355a59b2fb6e8ac2ed43d011440e66bc
David Quiroga <de.quiroga2@uniandes.edu.co>
2025-12-03 13:05:31 -0500

    Scroll

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,18 +10,32 @@ import Solutions from "@/pages/solutions";
 import Industries from "@/pages/industries";
 import Regulations from "@/pages/regulations";
 import ContactUs from "@/pages/contact-us";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/about-us" component={AboutUs} />
-      <Route path="/solutions" component={Solutions} />
-      <Route path="/industries" component={Industries} />
-      <Route path="/regulations" component={Regulations} />
-      <Route path="/contact-us" component={ContactUs} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <ScrollToTop />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about-us" component={AboutUs} />
+        <Route path="/solutions" component={Solutions} />
+        <Route path="/industries" component={Industries} />
+        <Route path="/regulations" component={Regulations} />
+        <Route path="/contact-us" component={ContactUs} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
